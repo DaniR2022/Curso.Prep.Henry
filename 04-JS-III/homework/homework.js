4d842b01fca0a7f757f253c6afb8f0e540b67b16
follow <follow@qq.com>
2021-11-29 20:16:36 -0300

    finalizado

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -147,7 +147,7 @@ let calculadora = 0;
 
   for (let i = 0; i < arreglo.length ; i++) {
 
-    if(arreglo[i] > 19){
+    if(arreglo[i] > 18){
 
       calculadora++
     }
@@ -177,7 +177,7 @@ function empiezaConNueve(n) {
 
   let numero = n.toString()
 
-  if(numero.charAt(0) === "9"){
+  if(numero.charAt(0) == "9"){
     return true
   }
   else {
@@ -190,7 +190,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (var i =0 ; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
+    }
+  }
+  return true
 } 
 
 
@@ -199,13 +204,32 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray = [];
+  for(let i= 0; i<array.length; i++) {
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      nuevoArray.push(array[i]);
+    }
+  }
+  if(nuevoArray.length < 3) {
+    return "No se encontraron los meses pedidos";
+  }
+  else {
+      return nuevoArray;
+  }
 }
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
-  // Tu código:
+  // Tu código: 
+  const nuevoArray = []
+  for (var i =0; i<array.length; i++) {
+    if (array[i]>100) {
+      nuevoArray.push(array[i])
+    }
+  }
+return nuevoArray
 }
 
 
@@ -217,7 +241,29 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  
+  var agregados = [];
+  var suma = numero ;
+
+  for (var i =0; i<10; i++) {
+    suma = suma + 2;
+
+    if (suma === i) 
+      break
+    
+    else {
+      agregados.push(suma)
+    }
+  }
+  if (i < 10) {
+    return "Se interrumpió la ejecución"
+  }
+
+  else {
+    return agregados
+  }
 }
+
 
 
 function continueStatement(numero) {
@@ -227,6 +273,23 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+      
+  var agregados = [];
+  var suma = numero ;
+
+  for (var i =0; i<10; i++) {
+
+    if(i === 5) 
+      continue;
+    
+     
+    else {
+      suma = suma + 2;
+      agregados.push(suma)
+    }
+  
+ }
+ return agregados
 }
 
 
